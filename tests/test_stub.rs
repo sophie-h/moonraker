4aaaab86cd41d8a4bd9faa5b920fffe9b431b2ad
Sophie Herold <sophie@hemio.de>
2022-04-25 18:10:37 +0200

    Move api

--- tests/test_stub.rs
+++ tests/test_stub.rs
@@ -2,16 +2,8 @@ use moonraker::keyring;
 
 use std::collections::HashMap;
 
-/*
-fn main() {
-    println!("Hello, world!");
-    //x();
-    async_std::task::block_on(decrypt_new()).unwrap();
-}
-*/
-
 async fn get_key() -> Result<zeroize::Zeroizing<Vec<u8>>, keyring::Error> {
-    let keyring = keyring::Keyring::load_default().await?;
+    let keyring = keyring::api::Keyring::load_default().await?;
 
     let secret = [
         44, 173, 251, 20, 203, 56, 241, 169, 91, 54, 51, 244, 40, 40, 202, 92, 71, 233, 174, 17,
@@ -24,34 +16,10 @@ async fn get_key() -> Result<zeroize::Zeroizing<Vec<u8>>, keyring::Error> {
 }
 
 #[async_std::test]
-async fn test_collection() -> Result<(), keyring::Error> {
-    let key = get_key().await?;
-
-    let keyring = keyring::Keyring::load_default().await?;
-
-    for item_encrypted in &keyring.items {
-        let item = item_encrypted.clone().decrypt(&key).unwrap();
-        dbg!(&item);
-        let item_encrypted_2 = item.encrypt(&key).unwrap();
-        let item2 = item_encrypted_2.decrypt(&key).unwrap();
-        dbg!(&item2);
-    }
-
-    dbg!(keyring
-        .search_items(HashMap::from([("tag-name", "some-value")]), &key)
-        .unwrap());
-    dbg!(keyring
-        .search_items(HashMap::from([("tag-namex", "some-value")]), &key)
-        .unwrap());
-
-    Ok(())
-}
-
-#[async_std::test]
 async fn test_dump_load() {
     let key = get_key().await.unwrap();
 
-    let mut new_keyring = keyring::Keyring::new();
+    let mut new_keyring = keyring::api::Keyring::new();
     new_keyring.items.push(
         keyring::Item::new(
             String::from("My Label"),
@@ -63,32 +31,12 @@ async fn test_dump_load() {
     );
     new_keyring.dump("/tmp/test.keyring").await.unwrap();
 
-    let loaded_keyring = keyring::Keyring::load("/tmp/test.keyring").await.unwrap();
+    let loaded_keyring = keyring::api::Keyring::load("/tmp/test.keyring")
+        .await
+        .unwrap();
     let loaded_items = loaded_keyring
         .search_items(HashMap::from([("my-tag", "my tag value")]), &key)
         .unwrap();
 
     assert_eq!(*loaded_items[0].password(), "A Password".as_bytes());
 }
-
-/*
-fn x() {
-    libsecret::password_store_sync(
-        Some(&my_schema()),
-        HashMap::from([("tag-name", "some-value")]),
-        None,
-        "My Text",
-        "MySecretPassword",
-        gio::Cancellable::NONE,
-    )
-    .unwrap();
-}
-
-fn my_schema() -> libsecret::Schema {
-    libsecret::Schema::new(
-        "org.example.moonraker",
-        libsecret::SchemaFlags::NONE,
-        HashMap::from([("tag-name", libsecret::SchemaAttributeType::String)]),
-    )
-}
-*/
